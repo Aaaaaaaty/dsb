daedf7df29558668c271876a6bd3b0e0e01e4eab
幕希Lin <2103887953@qq.com>
2016-05-04 14:42:53 +0800

    last one

---
+++ fetch.income.php
@@ -0,0 +1,26 @@
+
+<?php
+$birthYear = $_GET['birthYear'];
+$month = $_GET['month'];
+//print_r($birthYear.$month);
+//连接数据库
+$conn = mysqli_connect('localhost','root','');
+mysqli_select_db($conn,'myapp');
+$result = mysqli_query($conn,'SELECT * FROM manage2');
+$result_num = mysqli_num_rows($result);
+$result_all =[];
+for($i=0;$i<$result_num;$i++){
+    $result_arr = mysqli_fetch_assoc($result);
+    if($birthYear == $result_arr['birthYear']){
+        if($month == $result_arr['month']){
+            array_push($result_all, $result_arr);
+        }
+    }
+}
+//print_r($result_all);
+//转换json格式
+$result_json = json_encode($result_all);
+echo $result_json;
+file_put_contents('pay.json',$result_json);
+
+?>
---
+++ manage2.php
@@ -0,0 +1,26 @@
+
+<?php
+//设置页面内容是html编码格式是utf-8
+header("Content-Type: text/plain;charset=utf-8");
+//header("Content-Type: application/json;charset=utf-8");
+//header("Content-Type: text/xml;charset=utf-8");
+//header("Content-Type: text/html;charset=utf-8");
+//header("Content-Type: application/javascript;charset=utf-8");
+$conn = mysqli_connect('localhost','root','');
+mysqli_select_db($conn,'myapp');
+$sort = $_POST['sort'];
+$birthYear = $_POST['birthYear'];
+$month = $_POST['month'];
+$days = $_POST['days'];
+$amount = $_POST['amount'];
+$comment = $_POST['comment'];
+mysqli_query($conn,"INSERT INTO manage2(sort,birthYear,month,days,amount,comment) VALUE ('$sort','$birthYear','$month','$days','$amount','$comment')");
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    search();
+} elseif ($_SERVER["REQUEST_METHOD"] == "POST"){
+    create();
+}
+function create(){
+    echo '{"success":true,"msg":"类别：' . $_POST["sort"] . ' 信息保存成功！"}';
+}
+?>
